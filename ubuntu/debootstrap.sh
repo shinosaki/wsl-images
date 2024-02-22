b2d48e53e7bc9eb061006ddc0c216fd6f237473d
Shinosaki / 篠崎 <88357168+shinosaki@users.noreply.github.com>
2024-02-22 03:54:58 +0000

    fix bug

--- ubuntu/debootstrap.sh
+++ ubuntu/debootstrap.sh
@@ -4,8 +4,9 @@ echo "DEB_SUITE: $DEB_SUITE"
 echo "DEB_REPO: $DEB_REPO"
 
 debootstrap \
-  --arch $ARCH $DEB_SUITE \
+  --arch $ARCH \
   --include=dbus-user-session \
+  $DEB_SUITE \
   ./work $DEB_REPO
 
 tar -C ./work -czf /build/ubuntu-$DEB_SUITE-$ARCH-$(date '+%Y%m%d').tar.gz .
