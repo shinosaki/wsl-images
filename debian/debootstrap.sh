f3fb70e4ce79117ee40fefb234c5018f90fae928
Shinosaki / 篠崎 <88357168+shinosaki@users.noreply.github.com>
2024-02-22 02:58:40 +0000

    debootstrap: include "dbus-user-session" packkage

--- debian/debootstrap.sh
+++ debian/debootstrap.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-debootstrap --arch $ARCH $DEB_SUITE ./work $DEB_REPO
+debootstrap \
+  --arch $ARCH $DEB_SUITE \
+  --include=dbus-user-session \
+  ./work $DEB_REPO
+
 tar -C ./work -czf /build/debian-$DEB_SUITE-$ARCH-$(date '+%Y%m%d').tar.gz .
