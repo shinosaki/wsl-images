8671cf657ab0ea85c7d11c111056c21c538fe83e
Shinosaki / 篠崎 <88357168+shinosaki@users.noreply.github.com>
2024-02-23 18:35:22 +0000

    fix

--- debian/debootstrap.sh
+++ debian/debootstrap.sh
@@ -5,7 +5,6 @@ echo "DEB_REPO: ${DEB_REPO}"
 echo "PRE_CONF: ${PRE_CONF}"
 
 OUTPUT_NAME="debian-${DEB_SUITE}-${ARCH}-$(date '+%Y%m%d')"
-## https://yabutan.com/posts/201116_bash_how_to_join_array
 INCLUDES="dbus,dbus-user-session,bash-completion,ca-certificates,curl,gnupg,locales,sudo,wget"
 
 debootstrap \
