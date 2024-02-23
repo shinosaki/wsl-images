d44364189a3abe1c4b62d6bc607565dca4657f0c
Shinosaki / 篠崎 <88357168+shinosaki@users.noreply.github.com>
2024-02-23 02:52:22 +0000

    support ubuntu preconfigure

--- ubuntu/debootstrap.sh
+++ ubuntu/debootstrap.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-echo "ARCH: $ARCH"
-echo "DEB_SUITE: $DEB_SUITE"
-echo "DEB_REPO: $DEB_REPO"
+echo "ARCH: ${ARCH}"
+echo "DEB_SUITE: ${DEB_SUITE}"
+echo "DEB_REPO: ${DEB_REPO}"
+echo "PRE_CONF: ${PRE_CONF}"
+
+OUTPUT_NAME="ubuntu-${DEB_SUITE}-${ARCH}-$(date '+%Y%m%d')"
+## https://yabutan.com/posts/201116_bash_how_to_join_array
+INCLUDES="dbus,dbus-user-session,bash-completion,ca-certificates,curl,gnupg,locales,sudo,wget"
 
 debootstrap \
-  --arch $ARCH \
-  --include=dbus-user-session \
-  $DEB_SUITE \
-  ./work $DEB_REPO
+  --arch ${ARCH} \
+  --include=${INCLUDES} \
+  ${DEB_SUITE} \
+  /work ${DEB_REPO}
 
-tar -C ./work -czf /build/ubuntu-$DEB_SUITE-$ARCH-$(date '+%Y%m%d').tar.gz .
+tar -C /work -czf /build/${OUTPUT_NAME}.tar.gz .
---
+++ ubuntu/preconf.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "ARCH: ${ARCH}"
+echo "DEB_SUITE: ${DEB_SUITE}"
+echo "DEB_REPO: ${DEB_REPO}"
+echo "PRE_CONF: ${PRE_CONF}"
+
+OUTPUT_NAME="ubuntu-${DEB_SUITE}-${ARCH}-$(date '+%Y%m%d')"
+
+mkdir /work
+tar -C /work -xzf /build/${OUTPUT_NAME}.tar.gz
+
+## https://stackoverflow.com/a/27921346
+cat << 'EOF' >> /work/etc/skel/.bashrc
+
+
+## User Settings
+# Keep working directory when cloning terminal in WSL
+if command -v wslpath &> /dev/null; then
+  PROMPT_COMMAND=${PROMPT_COMMAND:+"$PROMPT_COMMAND; "}'printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"'
+fi
+# system32
+export PATH="/mnt/c/Windows/system32:$PATH"
+EOF
+
+chroot /work /bin/bash << 'EOF'
+adduser --disabled-password --gecos "" user
+adduser user sudo
+
+echo -e "[boot]\nsystemd=true\n[user]\ndefault=user" > /etc/wsl.conf
+
+echo "Etc/UTC" > /etc/timezone \
+&& dpkg-reconfigure -f noninteractive tzdata
+
+## https://serverfault.com/a/801162
+LANG=en_US.UTF-8
+sed -i -e "s/# ${LANG} UTF-8/${LANG} UTF-8/" /etc/locale.gen \
+&& dpkg-reconfigure -f noninteractive locales \
+&& update-locale LANG=${LANG}
+EOF
+
+tar -C /work -czf /build/${OUTPUT_NAME}-preconf.tar.gz .
